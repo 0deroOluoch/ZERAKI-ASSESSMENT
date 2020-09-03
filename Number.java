c73e74201a665709b59fbe9a9c9f09655f83f7fc
Odero Oluoch <52481094+0deroOluoch@users.noreply.github.com>
2020-09-03 14:26:54 +0300

    Add files via upload
    This is the solution to the missing number challenge using the summation method.

---
+++ Number.java
@@ -0,0 +1,27 @@
+
+
+public class Number {
+
+    public static void main(String args[])
+    {
+    	int givenArray[] = { 1, 2, 4, 5, 6 };
+
+    	int n = givenArray.length +1;
+
+    	int sumOfN = (n * (n+1))/2;
+
+    	for(int i =0; i< givenArray.length; i++ ){
+    		sumOfN = sumOfN - givenArray[1];
+    	}
+
+    	System.out.println("The Missing Number from the array is:  "+ sumOfN)
+
+    }
+
+
+}
+
+
+
+
+
